c13c8f4332f06d918bc9be4a638c907879aa2175
ncastal <nick.cast89@gmail.com>
2019-12-04 19:40:08 -0500

    Added comments

--- PyBank/Main/main.py
+++ PyBank/Main/main.py
@@ -1,40 +1,44 @@
-import os
+import os #import libraries
 import csv
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__))) #change working directory to one where the python file is located
 
-budget_csv = os.path.join("..","Resources","budget_data.csv")
-output_csv = os.path.join("..","Output","budget_output.cvs")
+budget_csv = os.path.join("..","Resources","budget_data.csv") #giving the location for budget_data.csv
+budget_txt= os.path.join("..","Output","budget_output.txt") #setting location for budget output text file
 
 print("Financial Analysis")
 print("-------------------------")
 
-with open(budget_csv, newline="") as csvfile:
+with open(budget_csv, newline="") as csvfile: #reading budget_data.csv
     csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvfile)
-    month=0
-    total = 0
-    greatest_increase=0
-    greatest_decrease=0
+    csv_header = next(csvfile) #skipping header
+    month=0 #variable to count how many months are in the csv file
+    total = 0 #variable to give a sum of all profits and losses
+    greatest_increase=0 #varaible used to find the greatest increase in profits
+    greatest_decrease=0 #variable used to find the greatest decrease in profits
     for row in csvreader:
-        month=month+1
-        total=total+int(row[1])
-        if int(row[1]) > greatest_increase:
+        month=month+1 #finding the total amount of months
+        total=total+int(row[1]) #finding the sum of all profits and losses
+        if int(row[1]) > greatest_increase: #if statements to find greatest increases and decreases
             greatest_increase=int(row[1])
             greatest_increase_month=row[0]
         if int(row[1])<greatest_decrease:
             greatest_decrease=int(row[1])
             greatest_decrease_month=row[0]
-    avg=round(total/month,2)
+    avg=round(total/month,2) #calcuating the average and rounding it to two decimals
 
-    print(f"Total months : {month}")
+    print(f"Total months : {month}") #printing the results
     print(f"Total: ${total}")
     print(f"Average Change: ${avg}")
     print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
     print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
 
-with open(output_csv, 'w', newline='') as csvfile2:
+with open(budget_txt, 'w') as writer: #writing the info to a text file
     
-    csvwriter = csv.writer(csvfile2, delimiter=',')
-    csvwriter.writerow(['Total Months','Total', 'Average Change', 'Greatest Increase', 'Greatest Decrease'])
-    csvwriter.writerow([month,total,avg,greatest_increase,greatest_decrease])
+    writer.write("Financial Analysis\n")
+    writer.write("-------------------------\n")
+    writer.write(f"Total months : {month}\n")
+    writer.write(f"Total: ${total}\n")
+    writer.write(f"Average Change: ${avg}\n")
+    writer.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n")
+    writer.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})\n")
--- PyPoll/Main/main.py
+++ PyPoll/Main/main.py
@@ -1,23 +1,25 @@
-import os
+import os #importing libraries
 import csv
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-election_csv = os.path.join("..","Resources","election_data.csv")
+os.chdir(os.path.dirname(os.path.abspath(__file__))) #change working directory to one where the python file is located
+election_csv = os.path.join("..","Resources","election_data.csv") #giving the location for election_data.csv
+election_result = os.path.join("..","Output","election_result.txt") #setting location for election result text file
+
 
 print("Election Results")
 print("-------------------------")
 
-with open(election_csv, newline="") as csvfile:
+with open(election_csv, newline="") as csvfile: #reading election_data.csv
     csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvfile)
-    total_votes=0
-    khan_votes=0
+    csv_header = next(csvfile) #skipping header
+    total_votes=0 #variable to count how many total votes
+    khan_votes=0 #variables to count how many votes for each canidate
     correy_votes=0
     li_votes=0
     otooley_votes=0
-    for row in csvreader:
-        total_votes=total_votes+1
-        if row[2]=="Khan":
+    for row in csvreader: 
+        total_votes=total_votes+1 #counting total votes
+        if row[2]=="Khan": #If statements to count votes for each canidate
             khan_votes=khan_votes+1
         if row[2]=="Correy":
             correy_votes=correy_votes+1
@@ -25,19 +27,31 @@ with open(election_csv, newline="") as csvfile:
             li_votes=li_votes+1
         if row[2]=="O'Tooley":
             otooley_votes=otooley_votes+1  
-    canidates={"Khan":khan_votes,"Correy":correy_votes,"Li":li_votes,"O'Tooley":otooley_votes}
+    canidates={"Khan":khan_votes,"Correy":correy_votes,"Li":li_votes,"O'Tooley":otooley_votes} #creating a dictonary for the canidates and votes cast for them
     
-    percent_khan = round((khan_votes/total_votes)*100,2)
+    percent_khan = round((khan_votes/total_votes)*100,2) #calculating the percentage of the votes each candiate recieved
     percent_correy = round((correy_votes/total_votes)*100,2)
     percent_li = round((li_votes/total_votes)*100,2)
     percent_otooley=round((otooley_votes/total_votes)*100,2)
-    print(f"Total votes: {total_votes}")
+    print(f"Total votes: {total_votes}") #printing the results
     print("-------------------------")
     print(f"Khan: {percent_khan}% ({khan_votes})")
     print(f"Correy: {percent_correy}% ({correy_votes})")
     print(f"Li: {percent_li}% ({li_votes})")
     print(f"O'Tooley: {percent_otooley}% ({otooley_votes})")
     print("-------------------------")
-    winner = max(canidates, key=canidates.get)
+    winner = max(canidates, key=canidates.get) #determines the winner of election by finding the canidate with the most votes
     print(f"Winner: {winner}")
     print("-------------------------")
+
+with open(election_result, 'w') as writer: #wtiting the results to a text file
+    writer.writelines('Election Results\n')
+    writer.writelines(f"Total votes: {total_votes}\n")
+    writer.writelines("-------------------------\n")
+    writer.writelines(f"Khan: {percent_khan}% ({khan_votes})\n")
+    writer.writelines(f"Correy: {percent_correy}% ({correy_votes})\n")
+    writer.writelines(f"Li: {percent_li}% ({li_votes})\n")
+    writer.writelines(f"O'Tooley: {percent_otooley}% ({otooley_votes})\n")
+    writer.writelines("-------------------------\n")
+    writer.writelines(f"Winner: {winner}\n")
+    writer.writelines("-------------------------\n")
